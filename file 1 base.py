fb059b4b6cd2d27d68dfd27f1826ff6c671c5a93
zides526 <74696187+zides526@users.noreply.github.com>
2020-11-19 10:16:18 +0530

    Start 1
    work for errors

---
+++ file 1 base.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 19:36:10 2020
+
+@author: devag
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys 
+import mysql.connector as conn
+global df
+df=pd.read_csv("C:\\Users\\devag\\Downloads\\IP Classes XII 2020-21\\project Covid-19\\full_data.csv")
+#function to display main menu
+
+def main_menu():
+    print("**********************************")
+    print("Read data from file in different way")
+    print("1.read complete csv file")
+    print("2.reading complete file without index")
+    print("================")
+    print("Data Visualization")
+    print("3.Line chart")
+    print("4.Bar plot")
+    print("5.Pie chart")
+    print("6.Scatter chart")
+    print("================")
+    print("Apply data manipulation in the records of csv files")
+    print("7.Sorting the data as per your choice")
+    print("8.Read top and bottom records from file as per requirement")
+    print("9.Make the copy of CSV file")
+    print("10.Read the Specific columns")
+    print("************************************")
+    
+main_menu()
+
+def ReadCSV():
+    print("Reading data from CSV file")
+    df=pd.read_csv("C:\users\devag\Downloads\raw_data16")
+    print (df)
+    
